5a2e779bcb94a09975ec46b3a345e49e0f2d2539
Alexandrewerka <163120790+Alexandrewerka@users.noreply.github.com>
2024-05-26 20:08:44 -0300

    Add files via upload

--- T2 alpha 0.0.4.c
+++ T2 alpha 0.0.4.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include <locale.h>
+
 	char pal[] = "plchdr", chute[] = "plchdr";
-	int i, t, check;
-	int wsize = 5;
+	int i, t, check, ii;
+	int wsize;
 	
 	void printgreen(char chute[], int i) {
 		printf("\033[32m%c\033[0m", chute[i]);
@@ -20,10 +21,15 @@ for (i=0; i < wsize; i++){
 		if (pal[i] == chute[i]){
 			printgreen(chute, i);
 	}
-		else if (chute[i] == pal[0] ||chute[i] == pal[1] ||chute[i] == pal [2] ||chute[i] == pal[3] ||chute[i] == pal[4]) {
-			printyellow(chute, i);
+		else for (ii = 0; ii < wsize; ii++){
+  
+             if (pal[ii] == chute[i]){
+             printyellow(chute, i);
 		}
-			else printf("%c", chute[i]);
+  }
+			else {
+             printf("%c", chute[i]);
+             }
 	}
 	
 	if (i = wsize - 1){
@@ -49,23 +55,33 @@ for (i=0; i < wsize; i++){
 
 int main(int argc, char *argv[]) {
 	char pal[] = "plch", chute[] = "plch";
-	int i, t, wsize;
-	scanf("%s\n", pal);
+	int i, t, wsize, *p;
 	
+	print("Qual será a palavra a ser chutada?");
+	            scanf("%s\n", pal);
 	
-	
+    wsize =	strlen(pal);
+    	p = (*int) malloc(wsize, sizeof(int));
+    	
 	for(t=0;t<6;t++){
 		
 	printf("Insira seu chute:\n");	
-	scanf("%s\n", chute);
+	               scanf("%s\n", chute);
 
 	wordcheck(pal, chute, wsize, i);
 	wincheck(pal, chute, wsize, i);
-		if (check = 1){
-			printf("Você venceu!");
-			break;
+	
+    	if (check = 1){
+	
+    		printf("Você venceu!");
+	
+    		break;
 		}
 	
 }
+
+  if (check != 1) {
+            printf("Você perdeu, tente outra vez!");
+            }
 		return 0;
 }
